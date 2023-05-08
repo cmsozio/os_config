3c1d7883050bb602e171a8c05d1e480695438903
cmsozio <cmsozio25@gmail.com>
2023-05-08 17:12:37 -0400

    move powerlevel10k directory to proper location

--- scripts/setup_terminal.sh
+++ scripts/setup_terminal.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# Install zsh
 sudo apt install zsh -y
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# Install oh-my-zsh
+curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
 
+# PowerLevel10k Setup - terminal prompt setup
 git clone https://github.com/romkatv/powerlevel10k.git
+mv powerlevel10k $(HOME)/.oh-my-zsh/custom/themes/
+mv ../dotfiles/.zshrc $(HOME)/
