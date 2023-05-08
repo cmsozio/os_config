989f62c44c426bcd46de20652935418b4f32a209
cmsozio <cmsozio25@gmail.com>
2023-05-08 16:48:37 -0400

    terminal setup script start

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,1 +1,3 @@
 ssh-keygen -t ed25519 -C "cmsozio25@gmail.com"
+
+
---
+++ scripts/setup_terminal.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/romkatv/powerlevel10k.git
