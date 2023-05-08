fa804da893015beac471f869fdf46fa16deebca1
cmsozio <cmsozio25@gmail.com>
2023-05-08 16:54:31 -0400

    need to install zsh first

--- scripts/setup_terminal.sh
+++ scripts/setup_terminal.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo apt install zsh -y
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 git clone https://github.com/romkatv/powerlevel10k.git
